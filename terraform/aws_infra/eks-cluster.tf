5fce2ce28fc856778d09b4aad932928830a19083
chrisley75 <cley@paloaltonetworks.com>
2023-07-04 00:20:20 +0200

    Add github action workflow to delete infra

--- terraform/aws_infra/eks-cluster.tf
+++ terraform/aws_infra/eks-cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "19.0.4"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.24"
+  cluster_version = "1.27"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
