2e6f78de557be602b7af087927a4f9a327f26062
chrisley75 <cley@paloaltonetworks.com>
2023-07-03 23:33:49 +0200

    Update github action workflow deploy_infrastructure.yml

--- scripts/check_s3_bucket.sh
+++ scripts/check_s3_bucket.sh
@@ -3,7 +3,7 @@
 bucket_name=$1-s3-tfstate
 
 # Check if the bucket exists
-if aws s3api head-bucket --bucket "$bucket_name" 2>/dev/null; then
+if aws s3api head-bucket --bucket $bucket_name 2>/dev/null; then
     echo "The bucket '$bucket_name' exists."
     echo "No need to create it, it will be used to store infrastructure tfstate which will be created later"
 else
