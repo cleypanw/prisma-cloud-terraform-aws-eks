30a1406913b20ee3b7af4677156d90cac5e218bb
chrisley75 <cley@paloaltonetworks.com>
2023-07-04 16:33:46 +0200

    Change eks-cluster.tf

--- terraform/aws_infra/eks-cluster.tf
+++ terraform/aws_infra/eks-cluster.tf
@@ -7,7 +7,8 @@ module "eks" {
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = false
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = false
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
