53324c8f7d6cb6be0fde0afe3cb4271ed876aba7
chrisley75 <cley@paloaltonetworks.com>
2023-07-05 10:50:37 +0200

    Reconfigure s3 bucket

--- terraform/bucket_s3/s3.tf
+++ terraform/bucket_s3/s3.tf
@@ -7,7 +7,12 @@ resource "aws_s3_bucket" "s3-tfstate" {
 
 resource "aws_s3_bucket_public_access_block" "s3-tfstate" {
   bucket = aws_s3_bucket.s3-tfstate.id
-
   block_public_acls       = false
   block_public_policy     = false
+}
+
+resource "aws_s3_bucket_acl" "s3-tfstate" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3-tfstate]
+  bucket = aws_s3_bucket.s3-tfstate.id
+  acl    = "public-read"
 }
