6cf9b83dd4b5ea682e8bc0a4650ef5b444f087a9
chrisley75 <cley@paloaltonetworks.com>
2023-07-20 20:31:39 +0200

    Update3

--- terraform/aws_infra/data-ansible-inventory.tf
+++ terraform/aws_infra/data-ansible-inventory.tf
@@ -1,3 +1,4 @@
+# Generate file from template
 data "template_file" "ansible_inventory" {
   template  = "${file("${path.module}/template/ansible-inventory.tpl")}"
   vars      = {
@@ -5,9 +6,22 @@ data "template_file" "ansible_inventory" {
   }
 }
 
-# Upload inventory to S3
+data "template_file" "cluster_name" {
+  template  = "${file("${path.module}/template/cluster-name.tpl")}"
+  vars      = {
+    cluster_name = module.eks.cluster_name
+  }
+}
+
+# Upload objects (inventory + cluster) to S3
 resource "aws_s3_object" "ansible_inventory" {
   bucket  = local.s3_name
   key     = "ansible_inventory.ini"
   content = data.template_file.ansible_inventory.rendered
+}
+
+resource "aws_s3_object" "cluster_name" {
+  bucket  = local.s3_name
+  key     = "cluster_name.txt"
+  content = data.template_file.cluster_name.rendered
 }
