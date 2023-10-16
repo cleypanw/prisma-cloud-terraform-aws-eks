fa57a38160302bbeec79316c1b850b029c0877e7
chrisley75 <cley@paloaltonetworks.com>
2023-10-16 12:15:02 +0200

    AWS modules: Change to Git Hash to be compliant with CKV_TF_1 Policy

--- terraform/aws_infra/terraform.tf
+++ terraform/aws_infra/terraform.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.46.0"
+      #version = "~> 4.46.0"
+      version = "~> 3.72.0"
     }
 
     random = {
